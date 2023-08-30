9d777f115aad81461f4dd43991d82dccea6b22f1
Star <val2019522@gmail.com>
2023-08-30 19:49:46 +0300

    init

--- app/src/main/java/com/test/webview/MainActivity.java
+++ app/src/main/java/com/test/webview/MainActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.webkit.CookieManager;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Toast;
@@ -22,20 +24,31 @@ public class MainActivity extends AppCompatActivity {
         webView = (WebView) findViewById(R.id.web);
         String CookieValue = null;
 
-        CookieManager cookieManager = CookieManager.getInstance();
-//        String cookies = cookieManager.getCookie(url);
-//        if(cookies != null){
-//            String[] temp=cookies.split(";");
-//            for (String ar1 : temp ){
-//                if(ar1.contains(CookieName)){
-//                    String[] temp1=ar1.split("=");
-//                    CookieValue = temp1[1];
-//                }
-//            }
-//        }
-        String cookies = CookieManager.getInstance().getCookie(url);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setDomStorageEnabled(true);
+        webView.getSettings().setDatabaseEnabled(true);
+//        CookieManager.getInstance().removeAllCookies(null);
+        CookieManager.getInstance().setAcceptCookie(true);
+        CookieManager.getInstance().setAcceptThirdPartyCookies(webView, true);
+
+//        CookieManager.getInstance().setCookie(url,"cookies-state=accepted");
         webView.loadUrl(url);
-        Log.e("cookies", cookies);
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageFinished(WebView view, String url1) {
+                String cookies = CookieManager.getInstance().getCookie(url);
+                Log.e("cookies", cookies);
+                // Convert the cookies to a universal format
+                CookieManager.getInstance().setAcceptCookie(true);
+
+                super.onPageFinished(view, url1);
+            }
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                webView.loadUrl(url);
+                return super.shouldOverrideUrlLoading(view, request);
+            }
+        });
     }
 
 }
